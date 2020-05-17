f6409fe44031747750015ad190ba14f1ab4b39c1
Tanishq Joshi <58567550+tanishqj2005@users.noreply.github.com>
2020-05-17 19:07:42 +0530

    Uploading Model and Play files!

---
+++ model.py
@@ -0,0 +1,76 @@
+# Importing Required Modules:
+
+from PIL import Image, ImageGrab
+import numpy as np
+from numpy import asarray
+import time
+import pickle
+import keyboard 
+import math
+from sklearn.linear_model import LogisticRegression
+
+# Writing some essential functions:
+
+def whichKey():
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('down'):  # if key 'q' is pressed 
+            print('1')
+            return 1
+        elif keyboard.is_pressed('up'):
+            print('2')
+            return 2
+        elif keyboard.is_pressed('q'):
+            print('3')
+            return 3
+        else:
+            print('0')
+            return 0
+    except:
+        print('error!')
+
+# Defining the model:
+
+clf = LogisticRegression()
+
+# Writing the code for training the model:
+
+if __name__ == "__main__":
+    clf = LogisticRegression()
+    x = []
+    y = []
+    print('Game starting in 2 seconds...')
+    time.sleep(2)
+    while True:
+        key = whichKey()
+        if key == 3:
+            break
+        flatten_image = []
+        image = ImageGrab.grab().convert('L')
+        data = asarray(image) 
+        for i in data:
+            for j in i:
+                flatten_image.append(j)
+        flatten_image= np.array(flatten_image)
+        x.append(flatten_image)
+        y.append(key)
+    X = np.array(x)
+    Y = np.array(y)
+    no_of_images = X.shape[0]
+    batch_size = 3
+    iterations = int(math.floor(no_of_images/batch_size))
+    for i in range(iterations):
+        start = i*batch_size
+        end = (i+1)*batch_size
+        X_minibatch = X[start:end,:]
+        print(X_minibatch.shape)
+        Y_minibatch = Y[start:end]
+        print(Y_minibatch.shape)
+        clf.fit(X_minibatch,Y_minibatch)
+    # clf.fit(X,Y)
+
+    #open a file where you want to store the data:
+    file = open('model.pkl','wb')
+
+    #dump info into that file:
+    pickle.dump(clf,file)
+    file.close()
---
+++ play.py
@@ -0,0 +1,55 @@
+# Importing Required Modules:
+
+from PIL import Image, ImageGrab
+import numpy as np
+from numpy import asarray
+import time
+import pickle
+import pyautogui
+import keyboard 
+
+# Writing some essential functions:
+
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+
+def whichKey():
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('q'):
+            print('Quitting Game...')
+            return 3
+    except:
+        pass
+
+# Defining the model:
+
+file = open('model.pkl','rb')
+clf=pickle.load(file)
+file.close()
+
+# Writing the code for using the trained model to play the game:
+
+if __name__ == "__main__":
+    print('Game starting in 2 seconds...')
+    time.sleep(2)
+    while True:
+        if(whichKey() == 3):
+            break
+        else:
+            flatten_image = []
+            image = ImageGrab.grab().convert('L')
+            data = asarray(image) 
+            for i in data:
+                for j in i:
+                    flatten_image.append(j)
+            flatten_image= np.array(flatten_image)
+            x = np.array([flatten_image])
+            model_prediction = clf.predict(x)[0]
+            print(model_prediction)
+            if model_prediction ==1:
+                hit("down")
+
+            elif model_prediction == 2:
+                hit("up")
+    print('Thanks for playing the game :)')
